78149a3909c20d85dcf81db5e06dd6126dd093f6
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2023-12-04 15:40:02 -0300

    Update Ponteiros_Funções.c

--- Ponteiros_Funções.c
+++ Ponteiros_Funções.c
@@ -4,13 +4,25 @@ int soma (int a, int b) {
   return a + b;
 }
 
+int subtracao (int c, int d) {
+    return c - d;
+}
+
+int multiplicacao (int e, int f) {
+    return e * f;
+}
+
+int divisao (int g, int h) {
+    return g / h;
+}
+
 int calcula (int x, int y, int (*operacao)(int, int)) {
   return (*operacao)(x, y);
 }
 
 int main() {
-  int resultado = calcula(5, 3, soma);
-  printf("Resultado: ", resultado);
+  int resultado = calcula(5, 3, divisao);
+  printf("Resultado: %d", resultado);
 
   return 0;
 }
