17e9035ad0a08694a2efcaeedcd076ce6a76e831
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2023-12-04 15:27:20 -0300

    Create Ponteiros_Funções.c

---
+++ Ponteiros_Funções.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+int soma (int a, int b) {
+  return a + b;
+}
+
+int calcula (int x, int y, int (*operacao)(int, int)) {
+  return (*operacao)(x, y);
+}
+
+int main() {
+  int resultado = calcula(5, 3, soma);
+  printf("Resultado: ", resultado);
+
+  return 0;
+}
